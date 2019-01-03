734534b87f7c4d799dde432ef1e5f50ce9205f69
Pierre Migeon <pmigeon@e1z3r7p1.42.us.org>
2019-01-02 16:13:18 -0800

    new stuff

--- main.c
+++ main.c
@@ -6,38 +6,57 @@
 /*   By: pmigeon <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:10:42 by pmigeon           #+#    #+#             */
-/*   Updated: 2018/12/04 13:03:55 by pmigeon          ###   ########.fr       */
+/*   Updated: 2019/01/02 14:15:47 by pmigeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include "./libft/libft.h"
+#include <fcntl.h>
 
-void	valid_input()
+
+void	ft_print_piece(char *str)
 {
-	read(fd, ,)
-	
+	while(str)
+		write(1, str++, 1);
+}
 
+void	valid_input(int fd, char **str)
+{
+	char 	buf[22];
+	int 	bits_read;
+	int 	i;
+
+	i = 0;
+	while ((bits_read = read(fd, buf, 21)) > 19 && i < 1) 
+	{
+		buf[21] = '\0';
+		ft_putstr(buf);
+		i++;
+	}
 
 }
 
 int		main(int argc, char **argv)
 {
-	int fd;
-		
+	int 	fd;
+	char 	**stored_array;
 
 	if (argc != 2)
 	{
-		ft_putchar("usage: ./fillit input_file");
+		ft_putstr("usage: ./fillit input_file");
 		return (1);
 	}
 	if ((fd = open(argv[1], O_RDONLY)) < 0)
-			return (1)
-	if (!(stored_array = valid_input(fd)))
+			return (1);
+	valid_input(fd, stored_array);
+
+/*	if (!(valid_input(fd, stored_array)))
 	{
 		ft_putchar("error");
 		return (1);
 	}
 	ft_normalize(stored_array)
-
-
+*/
 	return (0);
 }
